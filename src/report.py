600d96f803fcfe8207397608dbb924b4b5c445f2
abilytics <kyara.delacruz@pucp.edu.pe>
2022-03-27 22:42:34 -0500

    2do intento

--- src/report.py
+++ src/report.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('..\DATA\Churn_Modelling.csv')  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_csv('../DATA/Churn_Modelling.csv')  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
 
 st.title("Reporte de fuga de clientes bancarios")  # add a title
