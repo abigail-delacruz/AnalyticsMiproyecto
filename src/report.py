3e48ead9c83873b2d0239ec496fe91b8809f65d5
abilytics <kyara.delacruz@pucp.edu.pe>
2022-03-27 22:38:39 -0500

    5

--- src/report.py
+++ src/report.py
@@ -1,30 +1,13 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-st.title('Reporte de Fuga de Clientes Bancarios')
+df = pd.read_csv("DATA\Churn_Modelling.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
-DATE_COLUMN = 'Age'
+st.title("Reporte de fuga de clientes bancarios")  # add a title
+st.write(df)  # visualize my dataframe in the Streamlit app
 
-@st.cache
-def load_data(nrows):
-
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-
-    fig, ax = plt.subplots()
+fig, ax = plt.subplots()
   df.hist(
     bins=8,
     column="Age",
@@ -36,10 +19,3 @@ st.subheader('Number of pickups by hour')
     ax=ax,
   )
   st.write(fig)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
