036c07fbb208f1739a0a52788b8cfabab5fff6b3
abilytics <kyara.delacruz@pucp.edu.pe>
2022-03-27 22:33:15 -0500

    3

--- src/report.py
+++ src/report.py
@@ -4,20 +4,15 @@ import numpy as np
 
 st.title('Reporte de Fuga de Clientes Bancarios')
 
-DATA_URL=uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(df)
 DATE_COLUMN = 'Age'
-DATE_COLUMN = 'Gender'
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
@@ -28,8 +23,19 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+
+    fig, ax = plt.subplots()
+  df.hist(
+    bins=8,
+    column="Age",
+    grid=False,
+    figsize=(8, 8),
+    color="#86bf91",
+    zorder=2,
+    rwidth=0.9,
+    ax=ax,
+  )
+  st.write(fig)
 
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
